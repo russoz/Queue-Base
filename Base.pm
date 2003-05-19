7064a2d7cc508cf366aa11e05531b9848999354a
FARKAS Arpad <arpadf@cpan.org>
2003-05-18 23:59:50 -0800

    initial import of Queue-Base 1.01 from CPAN
    git-cpan-module:   Queue-Base
    git-cpan-version:  1.01
    git-cpan-authorid: ARPADF
    git-cpan-file:     authors/id/A/AR/ARPADF/Queue/Queue-Base-1.01.tar.gz

---
+++ Base.pm
@@ -0,0 +1,184 @@
+
+package Queue::Base;
+
+$VERSION = "1.01";
+
+
+################################################################################
+
+# Public Interface
+
+################################################################################
+
+
+sub new
+{
+	my $class = shift();
+	my ($rElements) = shift();
+
+	my $obj = {};
+	bless($obj, $class);
+		
+	if (defined $rElements && ref($rElements) eq 'ARRAY') {
+		@{$obj->{'list'}} = @{$rElements};
+	}
+	else {
+		@{$obj->{'list'}} = ();
+	}
+	
+	return $obj;
+}
+
+
+sub add
+{
+	my $obj = shift();
+	
+	while (my $elem = shift)
+	{ 
+		push(@{$obj->{'list'}}, $elem);
+	}
+}
+
+
+sub size
+{
+	my $obj = shift();
+	
+	return scalar(@{$obj->{'list'}});
+}
+
+
+sub remove
+{
+	my $obj = shift();
+	my ($numOfElements) = shift;
+
+	if (wantarray())
+	{ 
+		my @removedElements = ();
+		if (! defined $numOfElements) {
+			$numOfElements = 1;
+		}
+
+		for (my $k = $numOfElements; $k > 0; $k--)
+		{
+			if (my $elem = shift(@{$obj->{'list'}}) ) {
+				push(@removedElements, $elem);
+			}
+			else {
+				last;
+			}
+		}
+
+		return @removedElements;
+	}
+	else {
+		my $elem = shift(@{$obj->{'list'}});
+		return $elem;
+	}
+}
+
+
+sub empty
+{
+	my $obj = shift();
+	
+	my $size = scalar(@{$obj->{'list'}});
+	$obj->{'list'} = [];
+	
+	return $size;
+}
+
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+Queue::Base - Simple OO style queue implementation.
+
+=head1 SYNOPSIS
+
+ use Queue::Base;
+
+ # construction
+ my $queue = new Queue::Base;
+ # or
+ my $queue = new Queue::Base(\@elements);
+ 
+ # add new element to the queue
+ $queue->add($element);
+ 
+ # remove an element from the queue
+ if (! $queue->empty()) {
+     my $element = $queue->remove();
+ }
+ # or
+ $element = $queue->remove();
+ if (defined $element) {
+     # do some processing here
+ }
+ 
+ # add/remove more than just one element
+ $queue->add($elem1, $elem2 ...)
+ # and
+ @elements = $queue->remove(5);
+
+=head1 DESCRIPTION
+
+The Queue::Base is a simple implementation for queue structures using an 
+OO interface. Provides basic functionality: nothing less - nothing more.
+
+=head1 METHODS
+
+=head2 Constructor
+
+=over
+
+=item new [ELEMENTS]
+
+Creates a new empty queue.
+
+ELEMENTS is an array reference with elements the queue to be initialized with.
+
+=back
+
+=head2 Methods
+
+=over
+
+=item add [LIST_OF_ELEMENTS]
+
+Adds the LIST OF ELEMENTS to the end of the queue.
+
+=item size
+
+Returns the size of the queue.
+
+=item remove [NUMBER_OF_ELEMENTS]
+
+In scalar context it return the first element from the queue.
+
+In scalar context it attempts to return the NUMBER_OF_ELEMENTS requested;
+when NUMBER_OF_ELEMENTS is not given, it defaults to 1.
+
+=item emtpy
+
+Empties the queue, and returns the size of the queue before the empty operation.
+
+=back
+
+=head1 CAVEATS
+
+The module works only with scalar values. If you want to use more complex 
+structures (and there's a big change you want that) please use references, 
+which in perl5 are implemented as scalars.
+
+=head1 AUTHOR
+
+Farkas Arpad <arpadf@spidernet.co.ro>
+
+=cut
---
+++ t/test.t
@@ -0,0 +1,156 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Test;
+BEGIN { plan tests => 5};
+
+use Queue::Base;
+@Queue::ISA = qw(Queue::Base);
+
+use File::Basename;
+use lib dirname(readlink($0)||$0);
+use TestUtil;
+
+use vars qw(@testElements);
+
+################################################################################
+
+
+@testElements = qw(one two three);
+
+
+TEST1: # simple add/remove (queue size)
+{
+	my $queue = new Queue;
+	if ($queue->size() != 0) {
+		ok(0);
+		last TEST1;
+	}
+	
+	$queue->add('one');
+	$queue->add('two');
+	$queue->remove();
+	$queue->add('three');
+
+	if ($queue->size() != 2) {
+		ok(0);
+		last TEST1;
+	}
+	
+	$queue->remove();
+	$queue->remove();
+	$queue->remove();
+	
+	if ($queue->size() != 0) {
+		ok(0);	
+	}
+	else {
+		ok(1);
+	}
+}
+
+
+TEST2: # simple add/remove (content)
+{ 
+	my $queue = new Queue;	
+
+	$queue->add('one');
+	$queue->add('two');
+	$queue->add('three');
+
+	my @readElements;
+	my $element = $queue->remove(); push(@readElements, $element);
+	$element = $queue->remove(); push(@readElements, $element);
+	$element = $queue->remove(); push(@readElements, $element);
+	
+	if (! match_structure(\@testElements, \@readElements)) {
+		ok(0);
+		last TEST2;
+	}
+
+	# should not add element - remove() returns undef
+	if (my $element = $queue->remove()) {
+		push (@readElements, $element);
+	}
+
+	if (! match_structure(\@testElements, \@readElements)) {
+		ok(0);
+		last TEST2;
+	}
+	else {
+		ok(1);
+	}
+}
+
+
+TEST3: # add/remove multiple elements
+{
+	my $queue = new Queue;
+	$queue->add('one', 'two');
+	$queue->add('three');
+
+	my @readElements;
+	push (@readElements, $queue->remove(2));
+	push (@readElements, $queue->remove());
+
+	if (! match_structure(\@testElements, \@readElements)) {
+		ok(0);
+		last TEST3;
+	}
+
+	# the queue is now empty
+	push (@readElements, $queue->remove());
+
+	if (! match_structure(\@testElements, \@readElements)) {
+		ok(0);
+		last TEST3;
+	}
+
+	# try to remove more nonexistent elements
+	push (@readElements, $queue->remove(5));
+
+	if (! match_structure(\@testElements, \@readElements)) {
+		ok(0);
+	}
+	else {
+		ok(1);
+	}
+}
+
+
+TEST4: # initializing the queue
+{
+	my $queue = new Queue(\@testElements);
+
+	my @readElements;
+	push (@readElements, $queue->remove($queue->size()));
+
+	if (! match_structure(\@testElements, \@readElements)) {
+		ok(0);
+	}
+	else {
+		ok(1);
+	}
+}
+
+
+TEST5: # empty the queue
+{
+	my $queue = new Queue(\@testElements);
+	$queue->remove();
+	
+	# the size before the empty operation
+	my $size = $queue->empty();
+	
+	if ($size != 2) {
+		ok(0);
+	}	
+	elsif($queue->size() != 0) {
+		ok(0);
+	}
+	else {
+		ok(1);
+	}
+}
+
